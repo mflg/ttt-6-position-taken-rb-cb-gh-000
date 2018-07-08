cc57c0d5a06014aa195c6b9491047d6fd728d660
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 01:27:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, position)
+  if board==nil || board[position]=="" ||  board[position]==" "
+    false
+  elsif
+    true
+  end
+end
