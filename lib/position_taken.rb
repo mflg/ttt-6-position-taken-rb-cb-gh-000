4a1e76a80acd93120e5d9ca081552ecd3dd47f0c
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 01:28:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position)
-  if board==nil || board[position]=="" ||  board[position]==" "
+  if board==nil || board[position]==nil || board[position]=="" ||  board[position]==" "
     false
   elsif
     true
